97bde0cb59b104c406c491852decbdf72ded79c6
superRice <1246333567@qq.com>
2025-04-23 23:48:48 +0800

    fix：vite配置出错，导致打包出问题

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@
  * @Author: superRice
  * @Date: 2025-04-23 14:36:40
  * @LastEditors: superRice 1246333567@qq.com
- * @LastEditTime: 2025-04-23 23:42:13
+ * @LastEditTime: 2025-04-23 23:48:16
  * @FilePath: /IronManVisualizer/vite.config.js
  * @Description:
  * Do your best to be yourself
  * Copyright (c) 2025 by superRice, All Rights Reserved.
  */
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
