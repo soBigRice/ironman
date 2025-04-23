0e6cf1a84f28a1ec2eb55fad5033cc855b18be99
superRice <1246333567@qq.com>
2025-04-23 23:46:03 +0800

    feat：添加初始化环境，配置自动部署脚本

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,33 @@
  * @Author: superRice
  * @Date: 2025-04-23 14:36:40
  * @LastEditors: superRice 1246333567@qq.com
- * @LastEditTime: 2025-04-23 14:42:56
+ * @LastEditTime: 2025-04-23 23:42:13
  * @FilePath: /IronManVisualizer/vite.config.js
- * @Description: 
+ * @Description:
  * Do your best to be yourself
- * Copyright (c) 2025 by superRice, All Rights Reserved. 
+ * Copyright (c) 2025 by superRice, All Rights Reserved.
  */
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: "0.0.0.0",
-  },
+// export default defineConfig({
+//   plugins: [vue()],
+//   server: {
+//     host: "0.0.0.0",
+//   },
+// });
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [vue()],
+    server: {
+      host: "0.0.0.0",
+      cors: true,
+    },
+    base: env.VITE_APP_BASE_URL || "/",
+    resolve: {},
+  };
 });
